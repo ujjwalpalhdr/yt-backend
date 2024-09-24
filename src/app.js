4dea8605455b8d6fc7ca0d751babac07048afee3
Ujjwal Pal <ujjwalpalhdr@gmail.com>
2024-09-24 21:54:33 +0530

    routes configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaire
+app.use("api/v1/users", userRouter);
 export default app;
